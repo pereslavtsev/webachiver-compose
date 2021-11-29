a8af2d737e4cec2e4ab1e533f3581e62c09f2371
Vitaly Pereslavtsev <dev@pereslavtsev.com>
2021-11-29 23:15:02 +0300

    fix(app): remove useless files

--- db/create-multiple-postgresql-databases.sh
+++ db/create-multiple-postgresql-databases.sh
@@ -21,8 +21,6 @@ function install_ossp() {
 EOSQL
 }
 
-echo "gfhgfhgfhgfh gfhg hgfhfg"
-
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
 	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
 	for db in $(echo "$POSTGRES_MULTIPLE_DATABASES" | tr ',' ' '); do
